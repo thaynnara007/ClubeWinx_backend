9c6d10e0dd42f2fb53b6800f6731768fb4ff5640
thaynnara <thaynnara.goncalves@ccc.ufcg.edu.br>
2021-02-27 12:58:47 -0300

    preparando ambiente

--- src/config/database.js
+++ src/config/database.js
@@ -1,21 +1,21 @@
 require('dotenv').config();
 
 module.exports = {
-  env: process.env.NODE_ENV || 'development',
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  username: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  dialect: process.env.DB_DIALECT,
-  options: {
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  dialect: process.env.DB_DIALECT,
-  pool: {
-    max: 5,
-    min: 0,
-    idle: 10000,
-  },
-  },
+    env: process.env.NODE_ENV || 'development',
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    dialect: process.env.DB_DIALECT,
+    options: {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: process.env.DB_DIALECT,
+        pool: {
+          max: 5,
+          min: 0,
+          idle: 10000,
+        },
+    },
 };
