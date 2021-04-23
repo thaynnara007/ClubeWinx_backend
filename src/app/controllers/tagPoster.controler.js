64ae9a73b087240f2ee285dc5adc221f77e9285e
Gabriel Valentino <gabriel.alves@ccc.ufcg.edu.br>
2021-04-22 22:42:25 -0300

    TagPoster service

---
+++ src/app/controllers/tagPoster.controler.js
@@ -0,0 +1,127 @@
+const httpStatus = require('http-status-codes');
+const log = require('../services/log.service');
+const service = require('../services/tagpPoster.service');
+const posterService = require('../services/poster.service');
+
+const { StatusCodes } = httpStatus;
+
+const addTags = async (req, res) => {
+  try {
+    const { user } = req;
+    const { tags } = req.body;
+
+    log.info(`Inicializando adição de tags ao poster. userId=${user.id}`);
+
+    let poster = await posterService.getByUserId(user.id);
+
+    if (!poster) {
+      return res
+        .status(StatusCodes.NOT_FOUND)
+        .json({ error: 'Perfil não encontrado' });
+    }
+
+    log.info(`Relacionando as tags ao perfil. posterId=${poster.id}`);
+    await service.addTags(poster.id, tags);
+
+    poster = await posterService.getById(user.id, false);
+
+    log.info('Cadastro das tag realizado com sucesso');
+
+    return res.status(StatusCodes.OK).json(poster);
+  } catch (error) {
+    const errorMsg = 'Erro ao cadastrar tags ao perfil';
+
+    log.error(
+      errorMsg,
+      'app/controllers/tagposter.controller.js',
+      error.message,
+    );
+
+    return res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .json({ error: `${errorMsg} ${error.message}` });
+  }
+};
+
+const createTags = async (req, res) => {
+  try {
+    const { user } = req;
+    const { tags } = req.body;
+
+    log.info(
+      `Inicializando adição das tags criadas pelo usuário ao poster. userId=${user.id}`,
+    );
+
+    let poster = await posterService.getByUserId(user.id);
+
+    if (!poster) {
+      return res
+        .status(StatusCodes.NOT_FOUND)
+        .json({ error: 'Perfil não encontrado' });
+    }
+
+    log.info(`Relacionando as tags ao perfil. posterId=${poster.id}`);
+    await service.createTags(poster.id, tags);
+
+    poster = await posterService.getById(user.id, false);
+
+    log.info('Cadastro das tag realizado com sucesso');
+
+    return res.status(StatusCodes.OK).json(poster);
+  } catch (error) {
+    const errorMsg = 'Erro ao adicionar tags criadas pelo usuário ao perfil';
+
+    log.error(
+      errorMsg,
+      'app/controllers/tagposter.controller.js',
+      error.message,
+    );
+
+    return res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .json({ error: `${errorMsg} ${error.message}` });
+  }
+};
+
+const removeTags = async (req, res) => {
+  try {
+    const { user } = req;
+    const { tags } = req.body;
+
+    log.info(`Inicializando remoção das tags do poster. userId=${user.id}`);
+
+    let poster = await posterService.getByUserId(user.id);
+
+    if (!poster) {
+      return res
+        .status(StatusCodes.NOT_FOUND)
+        .json({ error: 'Perfil não encontrado' });
+    }
+
+    log.info(`Removendo as tags do perfil. posterId=${poster.id}`);
+    await service.removeTags(poster.id, tags);
+
+    poster = await posterService.getById(user.id, false);
+
+    log.info('Remoção das tag realizado com sucesso');
+
+    return res.status(StatusCodes.OK).json(poster);
+  } catch (error) {
+    const errorMsg = 'Erro ao remover tags do perfil';
+
+    log.error(
+      errorMsg,
+      'app/controllers/tagposter.controller.js',
+      error.message,
+    );
+
+    return res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .json({ error: `${errorMsg} ${error.message}` });
+  }
+};
+
+module.exports = {
+  addTags,
+  removeTags,
+};
